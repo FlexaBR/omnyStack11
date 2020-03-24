a9e735945e94daff1558acea50e00c2f0467a573
FlexaBR <r.gutheil@terra.com.br>
2020-03-24 20:30:44 -0300

    First

---
+++ backend/src/database/connection.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+const knex = require('knex');
+const configuration = require('../../knexfile');
+
+const connection = knex(configuration.development);
+
+module.exports = connection;
